0654047821c079b4fada547caf2b7403062fa774
Enes ÖZCAN <enesozcan@hotmail.com>
2021-04-23 21:42:55 +0300

    Proje dosyası ekle.

---
+++ DataAccessLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface ICategoryDal:IRepository<Category>
+    {
+      
+
+        // void Insert(Category p); ekleme yapılacak ancak bir parametreye ihtiyaç var bu nedenele category sınıfından gelen bir p parametresi olacak. bu p paramtresi sayesinde category sınıfındaki tüm prob'lara erişim sağlanabilecek
+
+
+
+    }
+}
---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    // burada tanımlanacak prob'lar sql'de tablo adı olarak karşılık bulacak
+    public class Context: DbContext  //: bir başka sınıftan bişeyelerin kalıtsal yolla alınması anlamına geliyor.
+    {
+        // bir başka katmanın prob'larını kullanmak istersen öncelikle o katmanları referans olarak eklemek gerekir. Bunun için Başvuruların (referances) üzerine sağ tıkla ve yeni başvuru ekle
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Heading> Headings { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+    }
+}
